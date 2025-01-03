cd1e4b5703c9b99e1e83608e43b9cd42042640e5
Amresh-Mishra <amresgmishra67099@gmail.com>
2025-01-03 15:17:28 +0530

    changes

--- frontend/src/pages/MainDashboard.jsx
+++ frontend/src/pages/MainDashboard.jsx
@@ -388,7 +388,7 @@ export default function Dashboard() {
                       onClick={handleLogout}
                       className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
                     >
-                      LogOut
+                      Logout
                     </button>
                   ) : (
                     ""
